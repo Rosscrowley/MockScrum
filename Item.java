db5ce14a9d0756b36f8b457a44c395aa9e87d63e
Rosscrowley <91560583+Rosscrowley@users.noreply.github.com>
2022-12-11 13:45:18 +0000

    Add files via upload

--- Item.java
+++ Item.java
@@ -4,12 +4,32 @@ public class Item {
 	private String name;
 	private String type;
 	private String exDate;
+	private int quantity;
+	private double vat;
 
-	public Item(String n, String t, String eD) {
+	public Item(String n, String t, String eD, int q, double vat) {
 		this.name = n;
 		this.type = t;
 		this.exDate = eD;
+		this.quantity = q;
+		this.vat = vat;
 
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public double getVat() {
+		return vat;
+	}
+
+	public void setVat(double vat) {
+		this.vat = vat;
 	}
 
 	public String getName() {
@@ -39,7 +59,12 @@ public class Item {
 	public String displayItem() {
 		// System.out.println("Student Name: " + this.name + " Address: " +
 		// this.address);
-		return "\n\nItem Name: " + name + "\n  Item Type " + type + "\n  Expiration Date " + exDate;
+		
+		String strQ = Integer.toString(quantity);
+		String strV = Double.toString(vat);
+		
+		return "\nItem Name: " + name + "\nItem Type: " + type + "\nExpiration Date: " + exDate + 
+				"\nItem quantity: " + strQ + "\nVAT: " + strV  ;
 	}
 
 }
--- home.java
+++ home.java
@@ -25,74 +25,78 @@ import java.util.*;
 
 public class home extends JFrame implements ActionListener {
 
-	
 	JButton view, exit, add, submitButton;
-	JTextField nameText, dateText;
+	JTextField nameText, dateText, quantityText;
 	JComboBox c1;
 	JTextArea viewItems;
-	ArrayList<Item> itemsList;
+	ArrayList<Item> itemsList = new ArrayList<Item>();
 
 	public home() {
 
 		JPanel panelAddItem = new JPanel();
-		
-		
+
 		view = new JButton("VIEW ITEMS");
 		exit = new JButton("EXIT");
 
 		JMenuBar menuBar = new JMenuBar();
-		
+
 		JLabel nameLabel = new JLabel("Item Name");
 		nameLabel.setBounds(10, 20, 80, 25);
 		panelAddItem.add(nameLabel);
-		
+
 		nameText = new JTextField(20);
 		nameText.setBounds(10, 10, 10, 10);
 		panelAddItem.add(nameText);
-		
+
 		JLabel dateLabel = new JLabel("Expiry date");
 		dateLabel.setBounds(10, 20, 80, 25);
 		panelAddItem.add(dateLabel);
-		
+
 		dateText = new JTextField(20);
 		dateText.setBounds(10, 10, 10, 10);
 		panelAddItem.add(dateText);
-				
-	    c1 = new JComboBox();
-	    c1.addItem("Essential");
-	    c1.addItem("Luxury");
-	    c1.addItem("Gift");
-	    panelAddItem.add(c1);
-	    
+
+		JLabel quantityLabel = new JLabel("Quantity");
+		quantityLabel.setBounds(10, 20, 80, 25);
+		panelAddItem.add(quantityLabel);
+
+		quantityText = new JTextField(20);
+		quantityText.setBounds(10, 10, 10, 10);
+		panelAddItem.add(quantityText);
+
+		c1 = new JComboBox();
+		c1.addItem("Essential");
+		c1.addItem("Luxury");
+		c1.addItem("Gift");
+		panelAddItem.add(c1);
+
 		submitButton = new JButton("Submit");
-		//submitButton.addActionListener(this);
+		// submitButton.addActionListener(this);
 		submitButton.setBounds(10, 80, 80, 25);
 		panelAddItem.add(submitButton);
-		
+
 		// ShowStudents text area displays the student array list
-				viewItems = new JTextArea();
-				viewItems.setEditable(true);
-	    
+		viewItems = new JTextArea();
+		viewItems.setEditable(true);
+
 		menuBar.add(view);
 		menuBar.add(exit);
-		
+
 		JPanel panelItem = new JPanel();
 		panelItem.setBorder(BorderFactory.createTitledBorder("NEW ITEM"));
 		panelItem.add(panelAddItem, BorderLayout.SOUTH);
 		panelItem.add(viewItems, BorderLayout.CENTER);
-		//panelNewStudent.add(panelModules, BorderLayout.EAST);
-		
-		//add.addActionListener(this);
+		// panelNewStudent.add(panelModules, BorderLayout.EAST);
+
+		// add.addActionListener(this);
 		view.addActionListener(this);
 		exit.addActionListener(this);
 		submitButton.addActionListener(this);
-	
-		
-		
+
 		this.setTitle("A Web Page");
 		this.setVisible(true);
-		this.setSize(600, 400);
-		//pnl.setBackground(Color.cyan);
+		this.setSize(800, 400);
+		// pnl.setBackground(Color.cyan);
 		this.add(panelItem);
 		this.setJMenuBar(menuBar);
 
@@ -104,33 +108,41 @@ public class home extends JFrame implements ActionListener {
 		if (e.getSource() == submitButton) { // when this button is pressed cutomers should be able to add something
 
 			// this.dispose();
-			//adding add = new adding();
-			
-			Item anItem;
-			
-			anItem = new Item(nameText.getText(), (String) c1.getSelectedItem(), dateText.getText());
-			
-			itemsList = new ArrayList<Item>();
+			// adding add = new adding();
 
-			itemsList.add(anItem);
+			Item anItem = null ;
+
+			int q = Integer.parseInt(quantityText.getText());
 			
-			for (Item s : itemsList) /** iterator */
-			{
-				//s.displayStudent();
-			 viewItems.setText( s.displayItem() );
+			
+			if (c1.getSelectedItem().equals("Essential")) {
+
+				anItem = new Item(nameText.getText(), (String) c1.getSelectedItem(), dateText.getText(), q, .10);
+			} else if (c1.getSelectedItem().equals("Luxury")) {
+
+				anItem = new Item(nameText.getText(), (String) c1.getSelectedItem(), dateText.getText(), q, .20);
+
+			} else if (c1.getSelectedItem().equals("Gift")) {
+
+				anItem = new Item(nameText.getText(), (String) c1.getSelectedItem(), dateText.getText(), q, .05);
 
 			}
-			
-			
-		}		
 
-		if (e.getSource() == view) { //// when this button is pressed cutomers should be able to view the products 
+			itemsList.add(anItem);
+			viewItems.append(anItem.displayItem());
 
-			//viewItems frame3 = new viewItems();
-			
-			
 		}
-		
+
+		if (e.getSource() == view) { //// when this button is pressed cutomers should be able to view the products
+
+			for (Item s : itemsList) /** iterator */
+			{
+				
+				viewItems.setText(s.displayItem());
+
+			}
+
+		}
 
 		if (e.getSource() == exit) {
 
@@ -142,7 +154,7 @@ public class home extends JFrame implements ActionListener {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	home f = new home();
+		home f = new home();
 
 	}
 }
